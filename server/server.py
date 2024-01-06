a2180d2081c0307a76eb79631e3ee585ab207e40
Pierce <123277032+0Pierce@users.noreply.github.com>
2024-01-06 02:12:26 -0500

    Added MongoDB connection

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from PyPDF2 import PyPDF2
 app = Flask(__name__)
 
 # Members API Route
@@ -9,3 +10,28 @@ def members():
 
 if __name__ == "__main__":
      app.run(debug=True)
+
+
+   
+
+
+def compressor(input, output):
+    # we open the file
+    with open(input, "rb") as file:
+        pdf_reader = PyPDF2.PdfFileReader(file)
+        pdf_writer = PyPDF2.PdfFileWriter(file)
+
+        for pageNumber in range(pdf_reader.getNumPages()):
+            page = pdf_reader.getPage(pageNumber)
+            page.compressContentStreams()
+            pdf_writer.addPage(page)
+            with open(output, 'wb') as compressed_file:
+                pdf_writer.write(compressed_file)
+
+
+if __name__ == "__main__":
+    input_path = "input.pdf"
+    output_path = "output_compressed.pdf"
+    compressor(input_path, output_path)
+
+
